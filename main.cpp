514dd6151c6784055054fd76df1e6616334670ea
Jason <thismart595@gmail.com>
2026-04-02 22:00:38 -0500

    Add initial implementation of Fall Detector application with Qt

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include "imusensor.h"
 
 int main(int argc, char *argv[])
 {
-    return 0;
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
 }
---
+++ main.qml
@@ -0,0 +1,11 @@
+import QtQuick
+import QtQuick.Controls
+
+Window {
+    id: root
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Fall Detector")
+    color: "black"
+}
